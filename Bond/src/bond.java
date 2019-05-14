470397661ed8c925ec25c37b6c779eb12a420de8
Mike Ross <34175985+boywonderstrikesback@users.noreply.github.com>
2019-05-14 12:14:11 -0400

    Add files via upload

---
+++ Bond/src/bond.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+import javax.swing.*;
+import java.util.Stack;
+
+
+
+public class bond {
+    
+public static void main(String args[]){  
+    
+   Scanner s=new Scanner(System.in); 
+    
+     
+   
+   System.out.print("Enter your name:");  
+   String firstname=s.next();  
+   String lastname=s.next(); 
+   
+   System.out.print( " "+firstname+" "+lastname +"  is gay" );  
+   s.close();  
+      
+      
+      
+}  
+    
+}
+
+
+
+
+
+
+
+
+
+
+        
+    
